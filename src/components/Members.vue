6a3da0c9fcc6b5b59f0ac42388219b1ee86a8c2d
Prakhar Mittal <prakharmittal@Prakhars-Macintosh.local>
2019-04-16 15:55:18 +0530

    Members Added Content

--- src/components/Members.vue
+++ src/components/Members.vue
@@ -7,70 +7,37 @@
             <div class="ppl">
                 <div class="row">
                     <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
+                        <div class="dp"><img src="../assets/members/ishan-manchanda.png"></div>
+                        <h1 class="name">Ishan Manchanda</h1>
+                        <h2 class="desig">Node President</h2>
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                <a href="https://github.com/Rippr" class="link" target="_blank"><img src="../assets/icons/github.png"></a>
                             </div>
                         </div>
                     </div>
                     <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
+                        <div class="dp"><img src="../assets/members/shubhro-gupta.png"></div>
+                        <h1 class="name">Shubhro Gupta</h1>
+                        <h2 class="desig">Node President</h2>
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                <a href="https://shubhro.in" class="link" target="_blank"><img src="../assets/icons/website.png"></a>
+                                <a href="https://github.com/shubhroGupta" class="link" target="_blank"><img src="../assets/icons/github.png"></a>
+                                <a href="https://behance.net/shubhroGupta" class="link" target="_blank"><img src="../assets/icons/behance.png"></a>
                             </div>
                         </div>
                     </div>
                     <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
+                        <div class="dp"><img src="../assets/members/siris-gupta.png"></div>
+                        <h1 class="name">Siris Gupta</h1>
+                        <h2 class="desig">Node President</h2>
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
-                        <div class="links-container">
-                            <div class="links">
-                                <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                
                             </div>
                         </div>
                     </div>
@@ -83,51 +50,102 @@
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                <a href="https://github.com/StrixRox" class="link" target="_blank"><img src="../assets/icons/github.png"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/prakhar-mittal.jpg"></div>
+                        <h1 class="name">Prakhar Mittal</h1>
+                        <h2 class="desig">Robotics Head</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                <a href="https://mittalprakhar.github.io" class="link" target="_blank"><img src="../assets/icons/website.png"></a>
+                                <a href="https://www.linkedin.com/in/prakhar-mittal-682ab1171/" class="link" target="_blank"><img src="../assets/icons/linkedin.png"></a>
+                                <a href="https://www.youtube.com/channel/UCLJcSqdNpNGFMwSD7-Sd_jg" class="link" target="_blank"><img src="../assets/icons/youtube.png"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/jainil-ajmera.jpeg"></div>
+                        <h1 class="name">Jainil Ajmera</h1>
+                        <h2 class="desig">Cryptic Lord</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                <a href="https://jainilajmera.github.io" class="link" target="_blank"><img src="../assets/icons/website.png"></a>
+                                <a href="https://bit.ly/jainilyoutube" class="link" target="_blank"><img src="../assets/icons/youtube.png"></a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
+                        <div class="dp"><img src="../assets/members/param-kapur.jpeg"></div>
+                        <h1 class="name">Param Kapur</h1>
+                        <h2 class="desig">Programming Head</h2>
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                <a href="https://github.com/paramkpr" class="link" target="_blank"><img src="../assets/icons/github.png"></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/dev-malik.png"></div>
+                        <h1 class="name">Dev Malik</h1>
+                        <h2 class="desig">Viewfinders President</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/kavi-chopra.png"></div>
+                        <h1 class="name">Kavi Chopra</h1>
+                        <h2 class="desig">Viewfinders President</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="member">
-                        <div class="dp"><img src="../assets/members/pratyush-kumar.jpeg"></div>
-                        <h1 class="name">Pratyush Kumar</h1>
-                        <h2 class="desig">Web Development Head</h2>
+                        <div class="dp"><img src="../assets/members/arnav-gupta.jpeg"></div>
+                        <h1 class="name">Arnav Gupta</h1>
+                        <h2 class="desig">Gaming Head</h2>
                         <div class="links-container">
                             <div class="links">
                                 <!-- <p class="heading">Links</p> -->
-                                <a href="" class="link"><img src="../assets/icons/face.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
-                                <a href="" class="link"><img src="../assets/icons/med.png"></a>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/garvdeep-singh.jpeg"></div>
+                        <h1 class="name">Garvdeep Singh</h1>
+                        <h2 class="desig">Digital Imaging Head</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="member">
+                        <div class="dp"><img src="../assets/members/jyotirmay-shelly.jpg"></div>
+                        <h1 class="name">Jyotirmay Shelly</h1>
+                        <h2 class="desig">Audio Head</h2>
+                        <div class="links-container">
+                            <div class="links">
+                                <!-- <p class="heading">Links</p> -->
+                                <a href="https://www.youtube.com/channel/UCbs9A1dU5IAz9p6mddV1H3A" class="link"><img src="../assets/icons/youtube.png"></a>
                             </div>
                         </div>
                     </div>
