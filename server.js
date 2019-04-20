f7f803b12fc53b285d415eba712ac2f39c3f636c
StrixROX <pk1234pratyushk@gmail.com>
2019-04-20 23:34:02 +0530

    Just Changed server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express()
+
+app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
 
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.PORT || 5000
+app.listen(port)
 
-console.log('server started at port ' + port);
+console.log('Server started on port ' + port)
