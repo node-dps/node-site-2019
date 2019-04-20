95d2438703d28f1b95ecda2a967ae17bfdbc0fb1
StrixROX <pk1234pratyushk@gmail.com>
2019-04-20 23:24:52 +0530

    I think I Fixed the Routing Issue

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
+const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started ' + port);
+console.log('server started at port ' + port);
